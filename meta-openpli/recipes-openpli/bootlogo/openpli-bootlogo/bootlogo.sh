9dc87c4b185a141fe0634ca17d0527f48138692c
openmb <miracleboxforum@gmail.com>
2014-11-28 10:47:49 +0100

    ..

--- meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
+++ meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
@@ -3,19 +3,18 @@
 # and set the correct videomode before showing the bootlogo
 [ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
-# We only support UNiBOX Official Boxes not others
-if ! [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-8000sv' ]; then
+# We only support MIRACLEBOX Official Boxes not others
+if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-8000sv' ]; then
+	BOOTLOGO=/usr/share/bootlogo.mvi
+else
 	BOOTLOGO=/usr/share/enigma2/box.mvi
 	/usr/bin/showiframe ${BOOTLOGO}
-	sleep 5
 	flash_erase /dev/mtd2 0 0
 	flash_erase /dev/mtd4 0 0
 	sync
 	nandwrite -p /dev/mtd4 /usr/share/enigma2/splashbox.bin
 	sync
 	reboot -f
-else
-	BOOTLOGO=/usr/share/bootlogo.mvi
 fi
 
 [ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
