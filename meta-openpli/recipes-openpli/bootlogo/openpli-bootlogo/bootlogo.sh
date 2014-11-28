79f9e2775caff3bde7be0f2ab739905aac8884b3
openmb <miracleboxforum@gmail.com>
2014-11-28 09:28:00 +0100

    Up logo

--- meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
+++ meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
@@ -5,9 +5,15 @@
 
 # We only support UNiBOX Official Boxes not others
 if ! [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-8000sv' ]; then
-	BOOTLOGO=/usr/share/bootlogo.mvi
-else
 	BOOTLOGO=/usr/share/enigma2/box.mvi
+	/usr/bin/showiframe ${BOOTLOGO}
+	sleep 5
+	flash_erase /dev/mtd/2 0 0
+	flash_erase /dev/mtd/4 0 0
+	sync
+	reboot -f
+else
+	BOOTLOGO=/usr/share/bootlogo.mvi
 fi
 
 [ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
