c157c6fbbdb1f532542fda8502b0f78dc4ed1b55
openmb <miracleboxforum@gmail.com>
2014-11-28 09:02:52 +0100

    logo

--- meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
+++ meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
@@ -3,6 +3,12 @@
 # and set the correct videomode before showing the bootlogo
 [ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
-BOOTLOGO=/usr/share/bootlogo.mvi
+# We only support UNiBOX Official Boxes not others
+if ! [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-8000sv' ]; then
+	BOOTLOGO=/usr/share/bootlogo.mvi
+else
+	BOOTLOGO=/usr/share/enigma2/box.mvi
+fi
+
 [ -f /etc/enigma2/bootlogo.mvi ] && BOOTLOGO=/etc/enigma2/bootlogo.mvi
 /usr/bin/showiframe ${BOOTLOGO}
