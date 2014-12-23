57e32e6eebb5b39dc729f48d81034cebf8c9433c
openmb <miracleboxforum@gmail.com>
2014-12-23 14:03:08 +0100

    miniplus

--- meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
+++ meta-openpli/recipes-openpli/bootlogo/openpli-bootlogo/bootlogo.sh
@@ -4,13 +4,14 @@
 [ -f /etc/videomode ] && cat /etc/videomode > /proc/stb/video/videomode
 
 # We only support MIRACLEBOX Official Boxes not others
-if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-8000sv' ]; then
+if [ "$(cat /proc/stb/info/boxtype)" == 'ini-1000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-2000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-5000sv' ] || [ "$(cat /proc/stb/info/boxtype)" == 'ini-8000sv' ]; then
 	BOOTLOGO=/usr/share/bootlogo.mvi
 else
 	BOOTLOGO=/usr/share/enigma2/box.mvi
 	/usr/bin/showiframe ${BOOTLOGO}
 	flash_erase /dev/mtd2 0 0
 	flash_erase /dev/mtd4 0 0
+	flash_erase /dev/mtd3 0 0
 	sync
 	nandwrite -p /dev/mtd4 /usr/share/enigma2/splashbox.bin
 	sync
